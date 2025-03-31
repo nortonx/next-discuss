7674c8da1d72dc6051f4a32fd1354757cb564cc3
Norton Almeida <norton.almeida@gmail.com>
2025-03-31 17:12:51 -0300

    path helpers

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -6,7 +6,7 @@ export default function Profile() {
   const session = useSession();
   
   if (session.data?.user) {
-    return <div>From client: user is signed in</div>;
+    return <div>From client: user is signed in {JSON.stringify(session.data.user)}</div>;
   }
 
   return <div>From client: user is NOT signed in</div>;
