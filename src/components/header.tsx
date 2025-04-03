6d08c21b6879be42000529c6eb35ac5472a47c7b
Norton Almeida <norton.almeida@gmail.com>
2025-04-03 14:48:16 -0300

    feat: implemented search input!

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,16 @@
+import { Suspense } from "react";
 import Link from "next/link";
 import {
   Navbar,
   NavbarBrand,
   NavbarContent,
-  Input,
+  NavbarItem,
 } from "@nextui-org/react";
-import HeaderAuth from "@/components/header-auth"
+import HeaderAuth from "@/components/header-auth";
+import SearchInput from "@/components/search-input";
+
 
 export default function Header() {
-  
 
   return (
     <Navbar className="shadow mb-6">
@@ -16,7 +18,11 @@ export default function Header() {
         <Link href="/" className="font-bold">Discuss</Link>
       </NavbarBrand>
       <NavbarContent justify="center">
-        <Input placeholder="Search" />
+        <NavbarItem>
+          <Suspense>
+            <SearchInput />
+          </Suspense>
+        </NavbarItem>
       </NavbarContent>
 
       <NavbarContent justify="end">
--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -1,13 +1,28 @@
 import type { Post } from "@prisma/client";
 import { db } from "@/db";
 
-export type PostWithData = (
-  Post & {
-    topic: { slug: string };
-    user: { name: string | null };
-    _count: { comments: number }
-  }
-)
+export type PostWithData = Post & {
+  topic: { slug: string };
+  user: { name: string | null };
+  _count: { comments: number }
+};
+
+export function fetchPostsBySearchTerm(term: string): Promise<PostWithData[]> {
+ return db.post.findMany({
+   include: {
+     topic: { select: { slug: true }},
+     user: { select: { name: true, image: true }},
+     _count: { select: { comments: true }}
+   },
+   where: {
+     OR: [
+       { title: { contains: term }},
+       { content: { contains: term }}
+     ]
+   }
+ });
+}
+
 
 export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
   return db.post.findMany({
